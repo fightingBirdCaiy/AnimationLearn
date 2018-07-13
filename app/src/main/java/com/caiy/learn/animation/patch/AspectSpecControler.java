8c9ba63afface5411825664247761a9069cbaafa
caiyong <caiyong@corp.netease.com>
2018-07-13 11:50:32 +0800

    aspectj init

---
+++ app/src/main/java/com/caiy/learn/animation/patch/AspectSpecControler.java
@@ -0,0 +1,33 @@
+package com.caiy.learn.animation.patch;
+
+import android.util.Log;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class AspectSpecControler {
+
+    private static final String TAG = "AspectSpecControler";
+
+    @Pointcut(value = "execution(* com.caiy.learn.animation..*.*(..)) && !execution(* com.caiy.learn.animation.patch..*.*(..))")
+    private void learnPointcut() {
+    }
+
+    @Around(value = "learnPointcut()")
+    public Object weaveApp(ProceedingJoinPoint joinPoint) throws Throwable {
+        return weavePatchLogic(joinPoint);
+    }
+
+    public Object weavePatchLogic(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long startT = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long consume = System.currentTimeMillis() - startT;
+        Log.i(TAG,"cost time：" + consume);
+        return proceed;
+    }
+
+}
---
+++ misc/src/main/java/com/caiy/learn/animation/misc/MiscUtil.java
@@ -0,0 +1,17 @@
+package com.caiy.learn.animation.misc;
+
+import android.util.Log;
+
+/**
+ * Created by caiyong on 2018/7/13.
+ */
+
+public class MiscUtil {
+
+    private static final String TAG = "MiscUtil";
+
+    public void test(){
+        Log.i(TAG,"test");
+    }
+
+}
